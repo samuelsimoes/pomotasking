119a6ba0995c6b54248abc75a1d11bfbe9429916
Samuel Simões <samuel@samuelsimoes.com>
2017-01-07 16:12:27 -0200

    starting the boilerplate

---
+++ src/js/popup.js
@@ -0,0 +1,9 @@
+import '../css/popup.css'
+import Greeting from './popup/greeting_component'
+import React from 'react'
+import { render } from 'react-dom'
+
+render(
+  <Greeting />,
+  window.document.getElementById('app-container')
+)
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack'),
+    path = require('path'),
+    fileSystem = require('fs'),
+    env = require('./utils/env'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    WriteFilePlugin = require('write-file-webpack-plugin')
+
+module.exports = {
+  entry: {
+    popup: path.join(__dirname, 'src', 'js', 'popup.js'),
+    background: path.join(__dirname, 'src', 'js', 'background.js')
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.(js|jsx)$/, loader: 'babel' },
+      { test: /\.css$/, loaders: ['style', 'css'] }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.css']
+  },
+  plugins: [
+    // expose and write the allowed env vars on the compiled bundle
+    new webpack.DefinePlugin({ 'process.env': JSON.stringify(env) }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'popup.html'),
+      filename: 'popup.html',
+      chunks: ['popup']
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'background.html'),
+      filename: 'background.html',
+      chunks: ['background']
+    }),
+    new WriteFilePlugin()
+  ]
+}
