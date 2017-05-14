596a49b7d39047509eff6623119e6b2a53aecb89
Samuel Simões <samuel@samuelsimoes.com>
2017-05-14 13:10:13 -0300

    Finished item notification actions
    resolves #3

--- src/js/background.js
+++ src/js/background.js
@@ -11,6 +11,7 @@ import migrator from './utils/migrator'
 import taskTile from './utils/taskTitle'
 import runningItemSchema from './schemas/runningItem'
 import { listenMessage, sendMessage } from './utils/radio'
+import { MAIN_EXECUTE_ACTION } from './constants/messages'
 import './main'
 
 migrator()
@@ -27,14 +28,59 @@ function renderMinutesBadge (leftTimeInSeconds, late) {
   })
 }
 
+let notificationID = 1
+
+let notifications = {}
+
+const SHORT_PAUSE_BUTTON_LABEL = 'Start a Short Pause'
+const LONG_PAUSE_BUTTON_LABEL = 'Start a Long Pause'
+const FINISH_PAUSE_BUTTON_LABEL = 'Finish the Pause'
+
+let pauseButtons = [
+  { title: FINISH_PAUSE_BUTTON_LABEL }
+]
+
+let buttons = {
+  [runnableTypes.POMODORO]: [
+    { title: SHORT_PAUSE_BUTTON_LABEL },
+    { title: LONG_PAUSE_BUTTON_LABEL }
+  ],
+  [runnableTypes.SHORT_PAUSE]: pauseButtons,
+  [runnableTypes.LONG_PAUSE]: pauseButtons
+}
+
+window.chrome.notifications.onClosed.addListener(notificationID => {
+  delete notifications[notificationID]
+})
+
+window.chrome.notifications.onButtonClicked.addListener((notificationID, buttonIndex) => {
+  let title = buttons[notifications[notificationID]][buttonIndex].title
+
+  if (title === SHORT_PAUSE_BUTTON_LABEL) {
+    sendMessage(MAIN_EXECUTE_ACTION, 'counter', 'startPause', [runnableTypes.SHORT_PAUSE])
+  } else if (title === LONG_PAUSE_BUTTON_LABEL) {
+    sendMessage(MAIN_EXECUTE_ACTION, 'counter', 'startPause', [runnableTypes.LONG_PAUSE])
+  } else if (title === FINISH_PAUSE_BUTTON_LABEL) {
+    sendMessage(MAIN_EXECUTE_ACTION, 'counter', 'finishItem')
+  }
+
+  window.chrome.notifications.clear(notificationID)
+})
+
 function notifyFinish (item) {
   let isPomodoro = (item.type === runnableTypes.POMODORO)
 
-  window.chrome.notifications.create(null, {
+  let currentNotificationID = (notificationID++).toString()
+
+  notifications[currentNotificationID] = item.type
+
+  window.chrome.notifications.create(currentNotificationID, {
     type: 'basic',
     title: `Pomotasking - Finished ${isPomodoro ? 'Pomodoro' : 'Pause'}`,
     message: `"${taskTile(item.description)}" finished.`,
-    iconUrl: 'pomotodo-notification.png'
+    iconUrl: 'pomotodo-notification.png',
+    priority: 1,
+    buttons: buttons[item.type]
   })
 
   let audio = new window.Audio('ring.ogg')
